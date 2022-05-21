6215d8f94b04b1aba728f49b59e8e4b19d6c6ad2
MaiVietCuong <s3682365@rmit.edu.vn>
2022-05-21 16:53:25 +0700

    delete img7

--- src/data/resources.js
+++ src/data/resources.js
@@ -7,7 +7,7 @@ import img1 from '../assets/images/img1.webp'
 import img4 from '../assets/images/img4.webp'
 import img5 from '../assets/images/img5.webp'
 import img6 from '../assets/images/img6.jpg'
-import img7 from '../assets/images/img7.JPG'
+
 import engine from '../assets/images/engine.jpeg'
 import allmakes from '../assets/images/all_makes.jpeg'
 import patternBackground from '../assets/images/hero-pattern-bg.png'
@@ -21,7 +21,6 @@ export default {
   img4: img4,
   img5: img5,
   img6: img6,
-  img7: img7,
   engine: engine,
   allmakes: allmakes,
   pattern: patternBackground,
